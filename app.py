48431378a464ecaa79cb56ec123b56dee2c6079a
shippo <shipporun@gmail.com>
2017-04-02 12:21:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import lask
+from flask import Flask
 
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'Flask Dockerized'
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(host='0.0.0.0')
 	
